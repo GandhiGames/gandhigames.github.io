aaf5daa68017bbadf65d254f72d930c4a2f1bfc6
GandhiGames <robert@Roberts-MacBook-Pro.local>
2015-06-04 16:20:52 +0100

    archived post

--- projects/SJM/SJM/src/com/cmt3319/interfaces/Audio.java
+++
@@ -1,7 +0,0 @@
-package com.cmt3319.interfaces;
-
-public interface Audio {
-    public Music newMusic(String filename);
-
-    public Sound newSound(String filename);
-}
--- projects/SJM/SJM/src/com/cmt3319/interfaces/Sound.java
+++
@@ -1,7 +0,0 @@
-package com.cmt3319.interfaces;
-
-public interface Sound {
-    public void play(float volume);
-
-    public void dispose();
-}
